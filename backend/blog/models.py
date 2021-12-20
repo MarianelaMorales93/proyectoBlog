db97805a31e5a2ec80e582f2f0caa4b8adff5d03
pabloalonso <pabloalonnso21@gmail.com>
2021-12-20 19:04:59 -0300

    Cree la tabla Posts

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    categoria=models.CharField(max_length=30)
+    fecha_publicacion=models.DateField()
+    
