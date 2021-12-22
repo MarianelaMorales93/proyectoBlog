635d9ceb161db68b9474d3a4adc04c0236056bf0
Victor Gustavo Esquivel <gusesquiveldevelops@gmail.com>
2021-12-21 22:11:59 -0300

    model.py

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,9 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
-#hola
+class profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Perfil de {self.user.username}'
 
 class Post(models.Model):
-    categoria=models.CharField(max_length=30)
-    fecha_publicacion=models.DateField()
-    
+    titulo = models.CharField('Título del Post',max_length = 150, unique = True)
+    slug = models.CharField('Slug', max_length = 150, unique = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    timestamp = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    categoria = models.CharField('Categoria', max_length = 150)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'
+
+class Comentario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comentario')
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'
